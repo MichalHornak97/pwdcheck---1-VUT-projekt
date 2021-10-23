a22106bd406e47e899ab4fd4dcfa67663460c01a
MichalHornak97 <hornak97@gmail.com>
2021-10-23 13:48:56 +0200

    new file:   pwcheck.c 	new file:   hesla.txt

---
+++ #include <stdio.h>.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+
+
+
+
+int main(int argc, char **argv[]){
+
+    if(argc=>1 && argc<5)
+    else(
+        fprintf(stderr,"Error - Nepovoleny pocet argumentu!")
+    )
+
+
+
+
+}
